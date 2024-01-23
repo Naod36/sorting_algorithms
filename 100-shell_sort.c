295962e263cc2a7a77ae0ccc1b05b03e8c82eba8
naod <naodk36@gmail.com>
2024-01-23 14:03:40 -0800

    Shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,22 +1,22 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
+ * swap_ints - swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
 {
-    int temp;
+	int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * shell_sort - Sort an array of integers in ascending order
- *              using the shell sort algorithm.
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
@@ -24,25 +24,25 @@ void swap_ints(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap, i, j;
+	size_t gap, i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (gap = 1; gap < (size / 3);)
-        gap = gap * 3 + 1;
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-    for (; gap >= 1; gap /= 3)
-    {
-        for (i = gap; i < size; i++)
-        {
-            j = i;
-            while (j >= gap && array[j - gap] > array[j])
-            {
-                swap_ints(array + j, array + (j - gap));
-                j -= gap;
-            }
-        }
-    }
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
 }
-
